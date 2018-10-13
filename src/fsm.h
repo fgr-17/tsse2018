12db8a5cd0ec00ba6e808359092b4621767c3a89
Federico G. Roux <rouxfederico@gmail.com>
2018-10-12 22:15:09 -0300

    Creo protot. de funcion: NO COMPILA

--- src/fsm.h
+++ src/fsm.h
@@ -3,6 +3,8 @@
 
 /** @brief prototipo de la funcion de inicializacion de la fsm */
 void fsmInit(void);
+/** @brief prototipo de la funcion de la fsm */
+void fsm (void);
 
 /** @brief variable de estado de la fsm */
 extern uint32_t estadoFsm;
