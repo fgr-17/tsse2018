93c5c085b14ffb8b3d221e4f6ef29b4059500124
Federico G. Roux <rouxfederico@gmail.com>
2018-10-12 21:44:34 -0300

    Test falla al cambiar el valor del estado inicial

---
+++ src/fsm.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <fsm.h>
+
+/** @brief prototipo de la funcion de inicializacion de la fsm */
+void fsmInit(void);
+
+/** @brief variable de estado de la fsm */
+uint32_t estadoFsm;
+
+/**
+ * @fn void fsmInit(void);
+ *
+ * @brief funcion que inicializa la máquina de estados
+ */
+
+void fsmInit(void) {
+
+}
+
+
+
--- src/fsm.h
+++ src/fsm.h
@@ -1,11 +1,11 @@
 /** @brief defino estado inicial */
-#define ESTADO_INICIAL  0
+#define ESTADO_INICIAL  0x100
 
 /** @brief prototipo de la funcion de inicializacion de la fsm */
 void fsmInit(void);
 
 /** @brief variable de estado de la fsm */
-uint32_t estadoFsm;
+extern uint32_t estadoFsm;
 
 
 
