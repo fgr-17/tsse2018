3b1c6ccce0c5f1e4ed395012270d14251b1ac414
Federico Roux <rouxfederico@gmail.com>
2018-10-17 15:50:24 -0300

    Agrego transiciones de comandos fuera de contexto. TEST PASA

--- test/test_fsm.c
+++ test/test_fsm.c
@@ -28,10 +28,17 @@ void test_fsmPrimerEstado(void) {
     fsmInit();
     TEST_ASSERT_EQUAL_HEX32(ESTADO_ESPERO_SINC, estadoFsm);
 
-    // Testeo que no se vaya a otro estado
+    // Testeo que no se vaya a otro estado con comandos validos
     recibirComando_ExpectAndReturn('S');
     fsm();
     TEST_ASSERT_EQUAL_HEX32(ESTADO_ESPERO_SINC, estadoFsm);
+    recibirComando_ExpectAndReturn('E');
+    fsm();
+    TEST_ASSERT_EQUAL_HEX32(ESTADO_ESPERO_SINC, estadoFsm);
+    recibirComando_ExpectAndReturn('P');
+    fsm();
+    TEST_ASSERT_EQUAL_HEX32(ESTADO_ESPERO_SINC, estadoFsm);
+
 
     // Testeo primer transición
     recibirComando_ExpectAndReturn('@');
@@ -51,18 +58,24 @@ void test_fsmSegundoEstado(void) {
     enviarRespuesta_Expect("U0000");
     fsm();
 
+    // Testeo que no se vaya a otro estado con comandos validos
+    recibirComando_ExpectAndReturn('E');
+    fsm();
+    TEST_ASSERT_EQUAL_HEX32(ESTADO_ESPERO_START, estadoFsm);
+    recibirComando_ExpectAndReturn('P');
+    fsm();
+    TEST_ASSERT_EQUAL_HEX32(ESTADO_ESPERO_START, estadoFsm);
     
+    // Pruebo transicion hacia si mismo
     recibirComando_ExpectAndReturn('@');    
     enviarRespuesta_Expect("U0000");
     fsm();
     TEST_ASSERT_EQUAL_HEX32(ESTADO_ESPERO_START, estadoFsm);
 
-
+    // Pruebo transicion hacia 3er estado
     recibirComando_ExpectAndReturn('S');    
     fsm();
-    TEST_ASSERT_EQUAL_HEX32(ESTADO_ETAPA_ONLINE, estadoFsm);
-
-
+    TEST_ASSERT_EQUAL_HEX32(ESTADO_ETAPA_ONLINE, estadoFsm);  
 
 }
 
